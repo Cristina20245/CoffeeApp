3ad8cca3a75a05cbd3974f1603e31db5355a03a4
Cristina <cristina.alvarez.f@gmail.com>
2025-01-04 10:08:11 +0100

    Primera versión del proyecto 2

--- Desktop/carreradecaballosm03uf5/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.carreradecaballosm03uf5 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.carreradecaballosm03uf5 to javafx.fxml;
-    exports org.example.carreradecaballosm03uf5;
-}
